d8435a9b4d86cbf5fd0954f38b41028dd1e69224
Martin Fernandes <martinijfb@gmail.com>
2024-06-03 16:45:25 +0100

    [Feat] Add delete functionality to ProgressGridView

--- ReflectHealth/Progress/ProgressGridView.swift
+++ ReflectHealth/Progress/ProgressGridView.swift
@@ -13,6 +13,9 @@ struct ProgressGridView: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: \TrackedData.date, order: .reverse) var trackedDataPieces: [TrackedData]
     
+    @State var showAlert: Bool = false
+    @State private var itemToDelete: TrackedData?
+    
     let columns: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible()),
@@ -26,6 +29,11 @@ struct ProgressGridView: View {
             }
             .padding()
         }
+        .alert("Delete Progress Item", isPresented: $showAlert, actions: {
+            alertActions
+        }, message: {
+            alertMessages
+        })
     }
     
     init(sort: SortDescriptor<TrackedData>, startDate: Date, endDate: Date) {
@@ -36,24 +44,52 @@ struct ProgressGridView: View {
 }
 
 extension ProgressGridView {
+    
+    // MARK: - VIEWS
     @ViewBuilder
     private func gridItem(_ trackedDataPiece: TrackedData) -> some View {
-        NavigationLink(value: trackedDataPiece) {
-            VStack(spacing: 0) {
-                if let image = UIImage(data: trackedDataPiece.image3) {
-                    ImageLabel(image: image, drawingData: trackedDataPiece.drawing3)
-                        .aspectRatio(1, contentMode: .fit)
+        ZStack(alignment: .topTrailing) {
+            NavigationLink(value: trackedDataPiece) {
+                VStack(spacing: 0) {
+                    if let image = UIImage(data: trackedDataPiece.image3) {
+                        ImageLabel(image: image, drawingData: trackedDataPiece.drawing3)
+                            .aspectRatio(1, contentMode: .fit)
+                    }
+                    dateText(trackedDataPiece.date)
                 }
-                dateText(trackedDataPiece.date)
+                .background(.lightBlue10)
+                .clipShape(RoundedRectangle(cornerRadius: 18))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 18).stroke(.lightBlue8, lineWidth: 1)
+                }
             }
-            .background(.lightBlue10)
-            .clipShape(RoundedRectangle(cornerRadius: 18))
-            .overlay {
-                RoundedRectangle(cornerRadius: 18).stroke(.lightBlue8, lineWidth: 1)
+            
+            Menu {
+                Button("Delete", systemImage: "trash") {
+                    itemToDelete = trackedDataPiece
+                    showAlert = true
+                }
+                NavigationLink(value: trackedDataPiece) {
+                    LabeledContent {
+                        Text("See your progress")
+                    } label: {
+                        Image(systemName: "person.text.rectangle")
+                    }
+                    
+                }
+                
+            } label: {
+                Image(systemName: "ellipsis.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24)
+                    .padding(4)
+                    .foregroundStyle(.white, .ultraThinMaterial)
             }
+            
         }
     }
-
+    
     @ViewBuilder
     private func dateText(_ date: Date) -> some View {
         VStack {
@@ -67,25 +103,52 @@ extension ProgressGridView {
         .lineLimit(1)
         .frame(maxWidth: .infinity)
     }
-
+    
     @ViewBuilder
     func ImageLabel(image: UIImage, drawingData: Data?) -> some View {
-            ZStack {
-                Group {
-                    Image(uiImage: image)
+        ZStack {
+            Group {
+                Image(uiImage: image)
+                    .resizable()
+                
+                if let safeData = drawingData, let drawingImage = UIImage(data: safeData) {
+                    Image(uiImage: drawingImage)
                         .resizable()
-                        .shadow(radius: 4, x: 4, y: 4)
-                    
-                    if let safeData = drawingData, let drawingImage = UIImage(data: safeData) {
-                        Image(uiImage: drawingImage)
-                            .resizable()
-                    }
                 }
-                .scaledToFill()
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding(8)
             }
+            .scaledToFill()
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding(8)
+        }
     }
+    
+    // MARK: - ALERT
+    func deleteTrackDataPiece(_ trackDataPiece: TrackedData) {
+        modelContext.delete(trackDataPiece)
+    }
+    
+    private var alertActions: some View {
+        Group {
+            Button(role: .destructive) {
+                if let itemToDelete = itemToDelete {
+                    deleteTrackDataPiece(itemToDelete)
+                }
+            } label: {
+                Text("Delete")
+            }
+            
+            Button(role: .cancel) {
+                itemToDelete = nil
+            } label: {
+                Text("Cancel")
+            }
+        }
+    }
+    
+    private var alertMessages: some View {
+        Text("Are you sure you want to delete this progress item?")
+    }
+    
 }
 
 #Preview {
